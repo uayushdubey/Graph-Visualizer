d89a548a80d605b2ad7301ac06658e93535956b1
Ayush Dubey <89792408+uayushdubey@users.noreply.github.com>
2024-08-21 11:58:56 +0530

    added all file from local repository

---
+++ ADA PBL/index.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const links = document.querySelectorAll('nav a');
+
+    links.forEach(link => {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            document.getElementById(targetId).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
